b35c6c5829ad16ef64c45ff10f5cb379cf2aff90
edward <edward.alexander9095@gmail.com>
2020-09-04 17:25:32 -0500

    subiendo repositorio

---
+++ app/src/main/java/edu/cibertec/fistclass/view/createAccountActivity.java
@@ -0,0 +1,16 @@
+package edu.cibertec.fistclass.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.cibertec.fistclass.R;
+
+public class createAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
